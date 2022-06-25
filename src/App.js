6cf3a44d345458b256d63ab940ab5ed2d0553b2d
ÇETİN <cetinnnarasss@gmail.com>
2022-06-25 16:19:27 +0300

    CONT.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import Card from "./components/card/Card";
-import data from "./util/data";
+import './App.scss'
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main';
 
-function App() {
-  //? console.log(data);
-  return (
-    <>
-    {data.map((item) => {
-      //!Destruction(ayırma) ,,map = dolaşma
-      const {id, img, btnName, dil} = item;
-      
-      return <Card key={id} img={img} btnName={btnName} dil={dil}/>
-      //key burda uniq bir değer ver ki hata vermesin.
-      //id yerine idex de kullanılabılır ama ıd kullanalım(mapde index kullanabılırsın)
-
-    
-    })}
-      
-    </>
-  );
+const App = () =>{
+  return(
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <Main/>
+    </div>
+  )
 }
 
 export default App;
